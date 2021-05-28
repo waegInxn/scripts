170734760689c84597c3a23e1ea242d41620f885
waegInxn <84984819+waegInxn@users.noreply.github.com>
2021-05-28 23:20:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
+from pyvirtualdisplay import Display
 import time
 
 # Press ⇧F10 to execute it or replace it with your code.
@@ -12,6 +13,9 @@ import time
 class AgreementGenerator:
 
     def __init__(self):
+        
+        display = Display(visible=0, size=(800, 800))  
+        display.start()
 
         opt = webdriver.ChromeOptions()
         opt.add_argument("no-sandbox")
